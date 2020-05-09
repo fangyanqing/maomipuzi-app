a274212a80a7d082dd837e070b4c8e6ac81b9ea0
fangyanqing <2637528271@qq.com>
2020-05-10 02:37:12 +0800

    设置首页

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>APP</div>
+    <div class="app-container">
+        <!-- 在这里放一个 Header  -->
+        <mt-header fixed title="猫咪铺子商城网站">
+          <span slot="left">
+            <mt-button icon="back">返回</mt-button>
+          </span>
+        </mt-header>
+
+        <!-- 路由的容器坑 -->
+        <router-view></router-view>
+
+        <!-- tabbar区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <router-link class="mui-tab-item" to="/home">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/news">
+                <span class="mui-icon mui-icon-chat"><span class="mui-badge">0</span></span>
+                <span class="mui-tab-label">消息</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/shopCart">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+                <span class="mui-tab-label">购物车</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </router-link>
+        </nav>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "App"
-    }
+    // export default {
+    //     name: "App"
+    // }
 </script>
 
 <style scoped>
-
+    .app-container {
+        padding-top: 40px;
+    }
 </style>
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import VueRouter from 'vue-router'
+
+// 导入路由组件
+import HomeContainer from './components/tabbars/HomeContainer.vue'
+import NewsContainer from './components/tabbars/NewsContainer.vue'
+import ShopCartContainer from './components/tabbars/ShopCartContainer.vue'
+import ContactContainer from './components/tabbars/ContactContainer.vue'
+
+// 创建路由对象
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/news', component: NewsContainer },
+    { path: '/shopCart', component: ShopCartContainer },
+    { path: '/contact', component: ContactContainer },
+  ],
+  linkActiveClass: 'mui-active' // 手动设置 被激活的 路由链接的高亮类名
+})
+
+export default router
